b4f28d90649aa1d34b077e622bcdfc474ce471af
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2022-02-06 10:10:52 -0600

    added database

--- database.sql
+++ database.sql
@@ -8,3 +8,37 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "category" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(255)
+);
+
+CREATE TABLE "project" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user",
+    "category_id" INT REFERENCES "category",
+    "date" date,
+    "budget" INTEGER,
+    "title" VARCHAR (300),
+    "user_notes" VARCHAR(20000)
+);
+
+CREATE TABLE "image" (
+    "id" SERIAL PRIMARY KEY,
+    "project_id" INT REFERENCES "project",
+    "url" VARCHAR(500),
+    "before_img" BOOLEAN,
+    "subtitle" VARCHAR(300)
+);
+
+CREATE TABLE "materials" (
+    "id" SERIAL PRIMARY KEY,
+    "project_id" INT REFERENCES "project",
+    "material" VARCHAR(300),
+    "qty" INTEGER,
+    "cost" DECIMAL,
+    "on_hand" BOOLEAN,
+    "location" VARCHAR(300)
+);
+
