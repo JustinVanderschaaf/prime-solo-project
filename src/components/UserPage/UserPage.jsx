599b11582824d67a583f8f9119a0aa20dd74ecd9
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2022-02-07 13:16:13 -0600

    upload with description working

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -6,54 +6,52 @@ import ProjectList from "../ProjectList";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 
+const AddProject = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const user = useSelector((store) => store.user);
 
+  // a local state to store the currently selected file.
+  const [selectedFile, setSelectedFile] = React.useState(null);
+  const [selectedDescription, setSelectedDescription] = React.useState('');
 
+  // display all projects on DOm upon app load
+  useEffect(() => {
+    dispatch({
+      type: "GET_PROJECT_PHOTOS",
+    });
+  }, []);
 
-  const AddProject = () => {
+  //Event handlers
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
 
-
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const user = useSelector((store) => store.user);
+  const handleSubtitle = (event) => {
+    setSelectedDescription(event.target.value);
+  };
+//End event handlers
+const itemToSend = {
+  subTitle: selectedDescription,
   
-    // display all projects on DOm upon app load
-    useEffect(() => {
-      dispatch({
-        type: "GET_PROJECT_PHOTOS",
-      });
-    }, []);
+}
 
-
-
-    const handleFileSelect = (event) => {
-      setSelectedFile(event.target.files[0])
-    }
-
-
-    // a local state to store the currently selected file.
-    const [selectedFile, setSelectedFile] = React.useState(null);
-    // const [selectedDescription, setSelectedDescription] = React.useState('');
-  
-    async function handleSubmit(event) {
-      event.preventDefault()
-      const formData = new FormData();
-
-      formData.append("selectedFile", selectedFile);
-      console.log('selected file is', selectedFile);
-      console.log('form data is,',formData);
-      
-        dispatch({ 
-            type: 'SEND_FILE',
-            payload: formData
-        });
+  async function handleSubmit(event) {
+    event.preventDefault();
     
-    };
-
-
-      
-
+    const formData = new FormData();
+    formData.append("description", selectedDescription)
+    formData.append("selectedFile", selectedFile);
     
 
+   console.log("item to send with selected file is", itemToSend);
+    console.log("form data is,", formData);
+
+    dispatch({
+      type: "SEND_FILE",
+      payload: formData
+    });
+  }
 
   return (
     <>
@@ -61,33 +59,31 @@ import axios from "axios";
         <h2>Welcome, {user.username}!</h2>
         <p>Your ID is: {user.id}</p>
       </div>
-      
-      <form onSubmit={handleSubmit} >
+
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
-        <input
+          <input
             type="file"
             className="form-control-file"
             name="uploaded_file"
             onChange={handleFileSelect}
           />
-          
-          {/* <input
+
+          <input
             type="text"
             className="form-control"
             placeholder="Description"
             name="description"
             onChange={handleSubtitle}
-          /> */}
-       
-         
+          />
 
-        <input type="submit" value="Upload File" />
+          <input type="submit" value="Upload File" />
         </div>
         <ProjectList />
       </form>
       <LogOutButton className="btn" />
     </>
   );
-}
+};
 
 export default AddProject;
