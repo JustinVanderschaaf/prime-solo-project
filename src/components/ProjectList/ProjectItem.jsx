91ead5955da010c4b63d36fcfab2d8d856f4dfe6
Justin Vanderschaaf <Vanderschaaf.usmc@gmail.com>
2022-02-14 10:14:38 -0600

    fixed rerender on change

--- src/components/ProjectList/ProjectItem.jsx
+++ src/components/ProjectList/ProjectItem.jsx
@@ -11,15 +11,18 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
+
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import Select from "@mui/material/Select";
-import InputLabel from "@mui/material/InputLabel";
-import FormControl from "@mui/material/FormControl";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import MenuItem from '@mui/material/MenuItem';
-import { useTheme } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+//end MUI
+
 //prop is project from projectList map
 function ProjectItem(prop) {
   const history = useHistory();
@@ -28,15 +31,8 @@ function ProjectItem(prop) {
   const user = useSelector((store) => store.user);
   const project = prop.project;
   const [editable, setEditable] = useState(false);
-  const [newTitle, setNewTitle] = useState("")
+  const [newTitle, setNewTitle] = useState("");
   //MUI
-  const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  }));
-  //end MUI
 
   const handleSelectProject = (project) => {
     // store selected movie object in Redux
@@ -53,76 +49,70 @@ function ProjectItem(prop) {
     setTogglePic(project.after_img);
   };
 
- 
-
   const editTitle = (event) => {
     event.preventDefault();
-    console.log("NewTitle is !!!", newTitle,project.id);
-   let titleToSend = {
-    newTitle: newTitle, 
-    project: project.id
-   }
+    console.log("NewTitle is !!!", newTitle, project.id);
+    let titleToSend = {
+      newTitle: newTitle,
+      project: project.id,
+    };
     dispatch({ type: "CHANGE_TITLE", payload: titleToSend });
-
   };
 
- 
-
+  console.log("rerendered");
   return (
-    <>
-      <Grid key={project.id}>
-        <Item>
-          <div>project owner: {project.username}</div>
-          <Card id="cards" sx={{ maxWidth: 300, minWidth: 300 }}>
-            <CardActions>
-              <Button onClick={afterImg} size="small">
-                After
-              </Button>
-              <Button onClick={beforeImg} size="small">
-                Before
-              </Button>
-              
-            </CardActions>
-            <CardMedia
-              component="img"
-              alt="NEW PROJECT"
-              height="300"
-              image={`uploads/${togglePic}`}
-              onClick={() => handleSelectProject(project)}
-            />
-            <CardContent>
-              <Typography variant="body2">
-              {editable === false ?
-            <h3>{project.title}</h3> :
-            <form onSubmit={editTitle}>
-            <input rows="4" cols="50" 
-                type="text"
-                value={newTitle}
-                onChange={(evt) => setNewTitle(evt.target.value)}
-                placeholder={project.title}
-               >
-            </input>
-            {user.id === project.user_id && (<button className="newProjectBtn" type="submit">
-          Submit
-        </button>)}
-            </form>
-        }
-              </Typography>
-              {user.id === project.user_id && (<button onClick={() => setEditable(true)}>Edit Details</button>)}
-              {user.id === project.user_id && (<button onClick={() => setEditable(false)}>Cancel</button>)}
-              
-            </CardContent>
-          </Card>
-          
-       
-        
-
-        
-        </Item>
-      </Grid>
-      
-    
-    </>
+    <Grid>
+      <Item>
+        <div>project owner: {project.username}</div>
+        <Card id="cards" sx={{ maxWidth: 300, minWidth: 300 }}>
+          <CardActions>
+            <Button onClick={afterImg} size="small">
+              After
+            </Button>
+            <Button onClick={beforeImg} size="small">
+              Before
+            </Button>
+          </CardActions>
+          <CardMedia
+            component="img"
+            alt="NEW PROJECT"
+            height="300"
+            image={`uploads/${togglePic}`}
+            onClick={() => handleSelectProject(project)}
+          />
+          <CardContent>
+            <div variant="body2">
+              {!editable ? (
+                <h3>{project.title}</h3>
+              ) : (
+                <form onSubmit={editTitle}>
+                  <input
+                    type="text"
+                    value={newTitle}
+                    onChange={(evt) => {
+                      console.log("on change", evt.target.value);
+                      setNewTitle(evt.target.value);
+                    }}
+                    placeholder={project.title}
+                  />
+                  {user.id === project.user_id && (
+                    <button className="newProjectBtn" type="submit">
+                      Submit
+                    </button>
+                  )}
+                </form>
+              )}
+            </div>
+            {user.id === project.user_id && (
+              <button onClick={() => setEditable(true)}>Edit Details</button>
+            )}
+            {user.id === project.user_id && (
+              <button onClick={() => setEditable(false)}>Cancel</button>
+            )}
+          </CardContent>
+        </Card>
+      </Item>
+    </Grid>
   );
 }
 
--- src/components/ProjectList/ProjectListCards.jsx
+++ src/components/ProjectList/ProjectListCards.jsx
@@ -4,12 +4,7 @@ import { useHistory } from "react-router-dom";
 import { useEffect } from "react";
 
 //MUI
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
